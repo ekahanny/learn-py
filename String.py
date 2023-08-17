d081f79151de2e61c7d13d64b3a9b4e08a95f68c
ekahanny <ekahnny@gmail.com>
2023-08-17 20:46:25 +0800

    menambahkan materi string width & allignment

--- String.py
+++ String.py
@@ -1,4 +1,4 @@
-## STRING ##
+# ## STRING ##
 
 data = "ini adalah string"
 print(data)
@@ -220,3 +220,41 @@ kanan = kanan.strip()
 print("'" + kanan + "'")
 tengah = tengah.strip('-')
 print("'" + tengah + "'")
+
+## STRING WIDTH & ALLIGNMENT ##
+
+# Width and Multiline
+data_nama = "Eka Hanny"
+data_umur = 20
+data_tinggi = 160
+data_nomor_sepatu = 41
+
+# string standard
+data_string = f"nama = {data_nama}, umur = {data_umur}, tinggi = {data_tinggi}, sepatu = {data_nomor_sepatu}"
+print(5*"="+"Data String Standard"+5*"=")
+print(data_string)
+
+# String multiline (dengan menggunakan enter, newline, \n)
+data_string = f"nama = {data_nama}, \numur = {data_umur}, \ntinggi = {data_tinggi}, \nsepatu = {data_nomor_sepatu}"
+print("\n"+5*"="+"Data String Multiline"+5*"=")
+print(data_string)
+
+# String multiline (kutip triplets)
+data_string = f"""nama = {data_nama}
+umur = {data_umur}
+tinggi = {data_tinggi}
+sepatu = {data_nomor_sepatu}
+"""
+print("\n"+5*"="+"Data String"+5*"=")
+print(data_string)
+
+# mengatur lebar
+# tanda >5 berarti memberi jarak ke kanan sebanyak 5
+data_string = f"""
+nama   = {data_nama:>5} 
+umur   = {data_umur}
+tinggi = {data_tinggi}
+sepatu = {data_nomor_sepatu}
+"""
+print("\n"+5*"="+"Data String"+5*"=")
+print(data_string)
