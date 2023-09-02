565269160762d8a5219226004bcf3f90241ccb09
ekahanny <ekahnny@gmail.com>
2023-09-03 00:48:47 +0800

    melatihan looping list

--- LoopingList.py
+++ LoopingList.py
@@ -37,23 +37,24 @@ list_buku = []
 
 while True:
     print("\nMasukkan Data Buku")
-    judul = input("Judul Buku: \t")
-    penulis = input("Penulis Buku: \t")
+    judul = input("Judul Buku: ")
+    penulis = input("Penulis Buku: ")
 
     buku_baru = [judul, penulis]
     list_buku.append(buku_baru)
 
     print("\n\n","="*10,"Data Buku","="*10)
+    
     for index,buku in enumerate(list_buku):
-        print(f"{index+1} | {buku[0] | {buku[1]}}")
+        print(f"{index+1} | {buku[0]} | {buku[1]}")
+    
     print("\n\n","="*20)
 
-    isLanjut = input("Apakah Dilanjutkan? (y/n)")
+    isLanjut = input("Apakah Dilanjutkan? (y/n) \n =>")
 
     if isLanjut == "n":
         break
-
-    print("PROGRAM SELESAI")
+print("PROGRAM SELESAI")
 
 
 
