d132da2e8e7ac3cb5b6e67a53c73dac1df1298f0
ekahanny <ekahnny@gmail.com>
2023-09-28 14:50:24 +0800

    menambahkan materi args, kwargs, dan type hints

--- Function.py
+++ Function.py
@@ -109,6 +109,9 @@ def hitung_pangkat(angka, pangkat=2):
 
 print(hitung_pangkat(2,4)) # Output 1
 
+##############################################
+
+
 # Keyword/Named Arguments
 
 '''
@@ -127,5 +130,127 @@ def fungsi_tambah(input1=1,input2=2,input3=3,input4=4):
 
 print(fungsi_tambah()) #10
 print(fungsi_tambah(input3=10)) #17
+
+##############################################
+
+# *args pada fungsi
+
+"""
+*args digunakan ketika ingin
+membuat function yang akan digunakan
+berulang kali. Dimana setiap kali
+function itu digunakan, function tsb
+cenderung memiliki parameter yang berbeda
+"""
+
+def fungsi(nama, tinggi, berat):
+    print(f"{nama} memiliki tinggi badan {tinggi} cm dan berat badan {berat} kg")
+fungsi("Kamu", 170, 40)
+
+def function(data_list):
+    data = data_list.copy()
+    nama = data[0]
+    tinggi = data[1]
+    berat = data[2]
+    print(f"{nama} memiliki tinggi badan {tinggi} cm dan berat badan {berat} kg")
+function(["Kamu", 170 , 20])
+
+def function(*args):
+    nama = args[0]
+    tinggi = args[1]
+    berat = args[2]
+    print(f"{nama} memiliki tinggi badan {tinggi} cm dan berat badan {berat} kg")
+function("Kamu", 170 , 30)
+
+# Studi Kasus
+def tambah(*data):
+    output = 0
+    for angka in data:
+        output = output + angka
+    return output
+
+hasil = tambah(1,2,3,4,5,6,7,8,9)
+print(f"Hasilnya adalah : {hasil}")
+
+hasil = tambah(10, 20, 5)
+print(f"Hasilnya adalah : {hasil}")
+
+# ##############################################
+
+# *kwargs pada fungsi
+
+"""
+kwargs menyimpan arguments 
+dalam bentuk dictionary
+
+"""
+
+contohDict = {
+    "nama" : "Eka",
+    "tinggi" : 160,
+    "berat" : 50
+}
+print(contohDict["nama"])
+
+def function(**kwargs):
+    name = kwargs["nama"]
+    height = kwargs["tinggi"]
+    weight = kwargs["berat"]
+    print(f"{name} memiliki tinggi badan {height} cm dan berat badan {weight} kg")
+
+function(nama="Kamu", tinggi="150", berat="20")
+
+# Studi Kasus
+
+def math(*args, **kwargs):
+    output = 0
+    if kwargs["option"] == "tambah":
+        for angka in args:
+            output +=angka
+    elif kwargs["option"] == "kali":
+        output = 1
+        for angka in args:
+            output *= angka
+    else:
+        print("tidak ada operasi yang tersedia")
+
+    return output
+
+hasil = math(1,2,3,4, option="tambah")
+print(f"hasil jumlah = {hasil}")
+
+hasil = math(1,2,3,4, option="kali")
+print(f"hasil kali = {hasil}")
+
+##############################################
+
+# Type Hints Pada Fungsi
+
+"""
+Digunakan untuk memudahkan
+dalam memahami kode
+
+"""
+
+# bentuk standar fungsi yang udah kita pelajari
+
+'''
+studi kasus
+def fungsi(parameter):
+    hasil = parameter**2
+    print(hasil)
+
+fungsi(1)
+fungsi("Eka")
+fungsi
+
+'''
+
+def sepuluh_pangkat(argument:int) -> int:
+    output = 10**argument
+    return output
+
+HASIL = sepuluh_pangkat(2)
+print(HASIL)
 
 
