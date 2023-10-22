e8fc3d2068954573fa4e72ae10aa3e8c4296fc53
ekahanny <ekahnny@gmail.com>
2023-10-22 20:02:27 +0800

    mengupdate materi regex

--- RegEx.py
+++ RegEx.py
@@ -56,6 +56,10 @@ stri = "bl!3BM"
 findstr = re.search(".[^0-9A-z]..", stri)
 print(findstr) # match -> l!3B
 
+text = "First line.\nSecond line."
+matches = re.findall(r".", text)
+print("Matches:", matches) #\n tidak akan terbaca
+
 # Backslash -> mencari literal karakter
 
 """
@@ -72,22 +76,61 @@ findstr = re.search("\.", stri)
 print(findstr) # match -> .
 
 
-## REGEX METACHARACTER ##
+# REGEX METACHARACTER ##
+
+# \A -> return match jika pola ditemukan pada awal string
+test_string = "Hello World!"
+find = re.search("\AHello", test_string)
+print(find) 
+
+# \b -> return match jika pola ditemukan pada awal atau akhir string
+test_string = "hey ho" # test diawal string
+find = re.search(r"\bhey", test_string)
+print(find)
+
+test_string = "ho hey" # test diakhir string
+find = re.search(r"\bhey", test_string)
+print(find)
+
+# \B -> return match jika pola ditemukan BUKAN pada awal atau akhir string
+test_string = "at, cat, hat, mat"
+matches = re.search(r'\Bat', test_string)
+print(matches)
 
 # \d -> digit character, shorthand for [0-9]
 string = "bla123bla"
 find = re.search("\d\d\d", string)
 print(find) # match -> 123
 
+# \D -> return match untuk karakter apapun selain digit
+test_string = "Umur saya 17 tahun"
+matches = re.findall(r'\D', test_string)
+print(matches)
+
+# \s -> whitespace/spasi
+string = "bla 123 bla"
+find = re.findall("\s", string)
+print(find)
+
+# \S -> return match untuk karakter apapun selain spasi
+test_string = "Umur saya 17 tahun"
+matches = re.findall(r'\S', test_string)
+print(matches)
+
 # \w -> shorthand for [A-Za-z0-9_]
 string = "@93h h7_Y ikkp"
 find = re.search("\w\w\w\w", string)
 print(find) # match -> h7_Y
 
-# \s -> whitespace/spasi
-string = "bla 123 bla"
-find = re.search("\s", string)
+# \W -> return match untuk karakter selain alfanumerik dan _
+string = "@93h h7_Y ikkp"
+find = re.search("\W", string)
 print(find)
+
+# \Z -> mengecek pola dimulai dari akhir string, jika sesuai maka mereturn match
+test_string = "Hello, World! This is the end"
+matches = re.search(r'end\Z', test_string)
+print(matches)
 
 # ^ -> start of string
 string = "Pinjam dulu 100"
@@ -155,6 +198,11 @@ print(re.search("colo{2,4}r", s2))
 print(re.search("colo{2,4}r", s3)) # None
 print(re.search("colo{2}r", s1))
 
+# | -> sama seperti operator OR
+test_string = "The color of the car is either blue or red."
+matches = re.findall(r'blue|red', test_string)
+print(matches)
+
 ## REGEX GROUPING ##
 
 # () -> grouping
@@ -196,9 +244,43 @@ panjang 4 karakter
 """
 print(re.findall("\d{4}", teks)) # match -> [1908, 1928, 1945]
 
+#re.split -> membagi sebuah string menjadi bagian kecil berdasarkan pola yg ditentukan
+text = "agar silaturahmi tidak terputus, pinjam dulu seratus"
+words = re.split(r'\s', text)
+print(words)
 
 #re.sub -> mengganti string yang match dgn string yang diberikan
 print(re.sub("\d{4}", "YEAR", teks))
+
+#re.subn -> 
+pattern = '\s'
+replace = '@'
+# variabel count merupakan unpacking pada tuple
+new_string, count = re.subn(pattern, replace, text)
+print(new_string)
+print("Jumlah pergantian : ", count)
+
+#re.match 
+"""
+Mencari sebuah pattern dimulai
+dari karakter pertama pada string.
+Jika karakter pertama tidak memenuhi
+pola, maka langsung mereturn none.
+Namun, jika karakter pertama memenuhi
+maka akan dilanjutkan pengecekan pola
+hingga selesai
+
+"""
+pattern = "a...s$"
+test_string = "abyss"
+result = re.match(pattern, test_string)
+print(result) # Match 
+
+pattern = "a...s$"
+test_string = "in a abyss"
+result = re.match(pattern, test_string)
+print(result) # None -> mulai dari karakter pertama tidak terpenuhi
+
 
 
 
